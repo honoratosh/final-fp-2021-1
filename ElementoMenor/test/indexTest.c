501f28803e305b7e6fd773b37207b265c68a42ac
Honorato Saavedra <honoratosaavedra@gmail.com>
2021-01-24 20:13:18 -0600

    Corrección para incluir el tamaño del arreglo.

--- ElementoMenor/test/indexTest.c
+++ ElementoMenor/test/indexTest.c
@@ -1,5 +1,5 @@
 int arreglo[] = {7,6,5,4,3,2};
-encontrarElementoMenor(arreglo), 2;
+encontrarElementoMenor(arreglo, 6), 2;
 
 int arreglo2[] = {-3,0,2,44,7,2};
-encontrarElementoMenor(arreglo2), -3
+encontrarElementoMenor(arreglo2, 6), -3
