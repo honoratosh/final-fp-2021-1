d2a74a2edb5b4a5f16f66a276c9dd56323a941f6
Honorato Saavedra <honoratosaavedra@gmail.com>
2021-01-24 20:15:57 -0600

    Corrección para incluir el tamaño del arreglo.

--- Interseccion/test/indexTest.c
+++ Interseccion/test/indexTest.c
@@ -1,9 +1,9 @@
 int primero[] = {5,9,2,4,6,0};
 int segundo[] = {4,2,7,8,4,5,0};
 int expResult[] = {5,2,4,0};
-encontrar(primero, segundo, expResult);
+encontrar(primero, 6, segundo, 7, expResult);
 
 int primero2[] = {22,89,4,10,33};
 int segundo2[] = {1,2,9};
 int expResult2[] = {};
-encontrar(primero2, segundo2, expResult2);
+encontrar(primero2, 5, segundo2, 3, expResult2);
