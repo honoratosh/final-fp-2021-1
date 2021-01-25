d4ad3348fbdd83cc92b87bf82ca029b5da78cde0
Honorato Saavedra <honoratosaavedra@gmail.com>
2021-01-24 20:08:08 -0600

    Corrección para incluir el tamaño del arreglo.

--- Autobus/test/indexTest.c
+++ Autobus/test/indexTest.c
@@ -1,6 +1,6 @@
 
 int cambios[][] = {{3, 0},{5, 1},{2, 1},{4, 3},{0, 8}};
-calcularPasajeros(cambios), 1
+calcularPasajeros(cambios,5), 1
 
 int cambios[][] = {{1, 0},{1, 0},{5, 1},{0, 3},{1, 1}};
-calcularPasajeros(cambios), 3
+calcularPasajeros(cambios,5), 3
