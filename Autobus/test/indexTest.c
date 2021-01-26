d56ccbdeb0b47de27e320957664eeaf6b21b6fac
honoratosh <hsaavedrah@iingen.unam.mx>
2021-01-26 10:22:36 -0600

    Ejemplo de prueba

--- Autobus/test/indexTest.c
+++ Autobus/test/indexTest.c
@@ -1,6 +1,39 @@
+/*
 
 int cambios[][] = {{3, 0},{5, 1},{2, 1},{4, 3},{0, 8}};
 calcularPasajeros(cambios,5), 1
 
 int cambios[][] = {{1, 0},{1, 0},{5, 1},{0, 3},{1, 1}};
 calcularPasajeros(cambios,5), 3
+
+*/
+
+#include "..\index.c"
+
+int main()
+{
+    
+    int cambios[][2] = {{3, 0},{5, 1},{2, 1},{4, 3},{0, 8}};
+    int cambios2[][2] = {{1, 0},{1, 0},{5, 1},{0, 3},{1, 1}};
+
+    if(calcularPasajeros(cambios, 5)==3)
+    {
+        printf("OK\n");
+    }
+    else
+    {
+        printf("Error\n");
+    }
+    
+    if(calcularPasajeros(cambios2, 5)==3)
+    {
+        printf("OK\n");
+    }
+    else
+    {
+        printf("Error\n");
+    }
+
+    return 0;
+}
+
